2c6f16c50bc6f7986187ca73c55dc8aa0849554e
SurKaa <suralcarywn@qq.com>
2023-07-14 10:54:15 +0800

    更改App.vue样式, 默认达到窗体的最大宽高, 中间router-view的剧中方

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <div id="app">
-
+    <router-view/>
   </div>
 </template>
 
-<script>
-
-</script>
-
 <style>
+* {
+  box-sizing: border-box;
+  outline: 0;
+}
+
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
