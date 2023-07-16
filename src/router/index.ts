eef64dfa5ee8fc8efa92803e5e5cd5eb457e50f8
SurKaa <suralcarywn@qq.com>
2023-07-16 08:43:39 +0800

    添加TodoList页面 并添加到路由根路径

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import VueRouter, {RouteConfig} from 'vue-router'
 import RegisterComponent from "@/views/RegisterPage.vue"
 import LoginComponent from "@/views/LoginPage.vue"
+import TodoListPage from "@/views/TodoListPage.vue";
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
+    {
+        path: '/',
+        component: TodoListPage
+    },
     {
         path: '/login',
         component: LoginComponent
---
+++ src/views/TodoListPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="todo-list">
+    <todo-item :data="data"/>
+  </div>
+</template>
+
+<script lang="ts">
+import TodoItem from "@/components/TodoItem.vue";
+import {Todo} from "@/ts/Todo";
+
+export default {
+  name: "TodoListPage",
+  components: {
+    TodoItem
+  },
+  data() {
+    return {
+      data: new Todo("No Title")
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
