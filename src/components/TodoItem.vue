a38ab14882e40c7cda6d23e3b865d917b645f27f
SurKaa <suralcarywn@qq.com>
2023-07-16 08:43:17 +0800

    添加视图模型Todo

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="todo-item">
+    {{data.title}}
+  </div>
+</template>
+
+<script lang="ts">
+import {Todo} from "@/ts/Todo";
+
+export default {
+  props: {
+    data: {
+      type: Todo,
+      require: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
