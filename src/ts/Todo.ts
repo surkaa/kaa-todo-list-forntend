bf5e7633115f6441d01e26e69ff42f7820eeec7c
SurKaa <suralcarywn@qq.com>
2023-07-18 16:57:46 +0800

    Todo.ts

--- src/ts/Todo.ts
+++ src/ts/Todo.ts
@@ -1,26 +1,59 @@
 export class Todo {
 
+    private _id: number;             // 笔记id
+    private _uid: number             // 创建这个笔记的用户id
+    private _flag: boolean;          // 是否完成
     private _title: string;          // 待办事项的标题
     private _description: string;    // 细节描述
-    private _flag: boolean;          // 是否完成
-    private _tag: string[];          // 标签
-    private _priority: number;       // 优先级
-    private _children: Todo[];       // 子任务
+    private _createTime: string;     // 创建时间
+    private _targetTile: string      // 预计完成时间按
+    private _updateTime: string      // 更新时间
+    private _noteStatus: number      // 笔记状态
 
     constructor(
+        id: number,
         title: string,
-        description: string = '',
+        uid: number = 0,
         flag: boolean = false,
-        tag: string[] = [],
-        priority: number = 1,
-        children: Todo[] = []
+        description: string = '',
+        createTime: string = '',
+        targetTile: string = '',
+        updateTime: string = '',
+        noteStatus: number = 0
     ) {
+        this._id = id;
+        this._uid = uid;
+        this._flag = flag;
         this._title = title;
         this._description = description;
-        this._flag = flag
-        this._tag = tag;
-        this._priority = priority;
-        this._children = children;
+        this._createTime = createTime;
+        this._targetTile = targetTile;
+        this._updateTime = updateTime;
+        this._noteStatus = noteStatus;
+    }
+
+    get id(): number {
+        return this._id;
+    }
+
+    set id(value: number) {
+        this._id = value;
+    }
+
+    get uid(): number {
+        return this._uid;
+    }
+
+    set uid(value: number) {
+        this._uid = value;
+    }
+
+    get flag(): boolean {
+        return this._flag;
+    }
+
+    set flag(value: boolean) {
+        this._flag = value;
     }
 
     get title(): string {
@@ -39,35 +72,35 @@ export class Todo {
         this._description = value;
     }
 
-    get flag(): boolean {
-        return this._flag;
+    get createTime(): string {
+        return this._createTime;
     }
 
-    set flag(value: boolean) {
-        this._flag = value;
+    set createTime(value: string) {
+        this._createTime = value;
     }
 
-    get tag(): string[] {
-        return this._tag;
+    get targetTile(): string {
+        return this._targetTile;
     }
 
-    set tag(value: string[]) {
-        this._tag = value;
+    set targetTile(value: string) {
+        this._targetTile = value;
     }
 
-    get priority(): number {
-        return this._priority;
+    get updateTime(): string {
+        return this._updateTime;
     }
 
-    set priority(value: number) {
-        this._priority = value;
+    set updateTime(value: string) {
+        this._updateTime = value;
     }
 
-    get children(): Todo[] {
-        return this._children;
+    get noteStatus(): number {
+        return this._noteStatus;
     }
 
-    set children(value: Todo[]) {
-        this._children = value;
+    set noteStatus(value: number) {
+        this._noteStatus = value;
     }
 }
