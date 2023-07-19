bfd2352e92c7c6d1e7c6433994380b6c57915d2f
SurKaa <suralcarywn@qq.com>
2023-07-19 17:56:36 +0800

    没有代办事项时的视图

--- src/views/TodoListPage.vue
+++ src/views/TodoListPage.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="todo-list">
-    <todo-item v-for="item in data.data" :todo="item" :key="item.id"/>
+    <el-empty class="todo-empty-view" v-if="isEmpty" description="暂无待办事项">
+      <el-button @click="reload">轻触刷新</el-button>
+    </el-empty>
+    <todo-item v-if="!isEmpty" v-for="item in data" :todo="item" :key="item.id"/>
   </div>
 </template>
 
@@ -9,27 +12,37 @@ import TodoItem from "@/components/TodoItem.vue";
 import router from "@/router";
 import request from '@/utils/request'
 import {Todo} from "@/ts/Todo";
+import {defineComponent} from "vue";
 
-export default {
+export default defineComponent({
   name: "TodoListPage",
   components: {
     TodoItem
   },
   data() {
     return {
+      isEmpty: true,
       data: [] as Todo[]
     }
   },
   created() {
     request.get('/todos').then((res: any) => {
-      this.data = res.data
       if (res.data.code > 0) {
         alert("请先登录后查看哦~")
         router.push('/login')
       }
+      this.data = res.data.data
+      if (this.data.length != 0) {
+        this.isEmpty = false
+      }
     })
+  },
+  methods: {
+    reload() {
+      location.reload()
+    }
   }
-}
+})
 
 </script>
 
@@ -76,5 +89,10 @@ export default {
 
 .todo-list::-webkit-scrollbar-thumb:hover {
   background-color: rgba(144, 147, 153, .3);
+}
+
+.todo-empty-view {
+  height: 100%;
+  opacity: 0.5;
 }
 </style>
