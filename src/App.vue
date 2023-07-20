98635101ad796dc3c09b1a20a0879cfac15f8223
SurKaa <suralcarywn@qq.com>
2023-07-20 15:50:16 +0800

    将style转到index.html

--- src/App.vue
+++ src/App.vue
@@ -35,14 +35,6 @@ export default defineComponent({
   outline: 0;
 }
 
-html {
-  font-size: 10px;
-}
-
-body {
-  margin: 0;
-}
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
